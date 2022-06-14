bf3fd9d23e30f69faf0a8a0cb921132511ff46e7
Michal <BlackDragon17@users.noreply.github.com>
2022-06-15 01:37:12 +0200

    Add pages change check

--- resources/scripts/gh-pages-deploy.js
+++ resources/scripts/gh-pages-deploy.js
@@ -18,8 +18,8 @@ async function scriptWin() {
         try {
             await execa("git", ["update-index", "--refresh"], shell);
         } catch (e) {}
-        const { stdout } = await execa("git", ["diff-index", "HEAD"], shell);
-        if (stdout) {
+        const mainDiff = await execa("git", ["diff-index", "HEAD"], shell);
+        if (mainDiff.stdout) {
             console.log("Stashing changes...\n");
             stashed = true;
             await execa("git", ["stash"], shell);
@@ -28,19 +28,24 @@ async function scriptWin() {
         console.log("Building...");
         await print(execa("npm", ["run", "build"]));
 
-        console.log("\nPushing...");
-        await print(execa("git", ["checkout", "--orphan", "gh-pages"], shell));
-        await print(execa("git", ["--work-tree", "dist", "add", "--all"], shell));
-        await print(execa("git", ["--work-tree", "dist", "commit", "-m", "Deploy"], shell));
-        await print(execa("git", ["push", "origin", "HEAD:gh-pages", "--force"], shell));
-
-        console.log("Successfully deployed");
+        console.log("\nChecking for changes...")
+        await print(execa("git", ["checkout", "gh-pages"], shell));
+        await execa("git", ["--work-tree", "dist", "rm", "--cached", "-r", "."], shell);
+        await execa("git", ["--work-tree", "dist", "add", "--all"], shell);
+        const pagesDiff = await execa("git", ["diff-index", "HEAD"], shell);
+        if (pagesDiff.stdout) {
+            console.log("\nPushing...");
+            await print(execa("git", ["--work-tree", "dist", "commit", "-m", "Deploy"], shell));
+            await print(execa("git", ["push"], shell));
+            console.log("Successfully deployed");
+        } else {
+            console.log("\nNo changes found!");
+        }
     } catch (e) {
         console.error(e);
         exitCode = 1;
     } finally {
-        await execa("git", ["checkout", "-f", "main"], shell);
-        await execa("git", ["branch", "-D", "gh-pages"], shell);
+        await print(execa("git", ["checkout", "-f", "main"], shell));
         if(stashed) {
             await execa("git", ["stash", "pop"], shell);
         }
