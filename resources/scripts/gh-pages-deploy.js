43b86a2c82c91c5d6ce1e1d315cfafa17f7a2b6b
Michal <BlackDragon17@users.noreply.github.com>
2022-06-14 23:29:02 +0200

    gh-pages-deploy script

---
+++ resources/scripts/gh-pages-deploy.js
@@ -0,0 +1,66 @@
+const os = require("node:os").type();
+const shell = {shell: "C:/Program Files/Git/bin/bash.exe"};
+
+async function print(promise) {
+    return promise.then(res => {
+        console.log(res.stderr);
+        console.log(res.stdout);
+    }, console.log);
+}
+
+// https://dev.to/the_one/deploy-to-github-pages-like-a-pro-with-github-actions-4hdg
+async function scriptWin() {
+    const {execa} = await import("execa");
+    let exitCode = 0;
+    try {
+        console.log("Building...");
+        await print(execa("npm", ["run", "build"]));
+
+        console.log("Pushing...");
+        await print(execa("git", ["checkout", "--orphan", "gh-pages"], shell));
+        await print(execa("git", ["--work-tree", "dist", "add", "--all"], shell));
+        await print(execa("git", ["--work-tree", "dist", "commit", "-m", "Deploy"], shell));
+        await print(execa("git", ["push", "origin", "HEAD:gh-pages", "--force"], shell));
+
+        console.log("Successfully deployed");
+    } catch (e) {
+        console.error(e);
+        exitCode = 1;
+    } finally {
+        await execa("git", ["checkout", "-f", "main"], shell);
+        await execa("git", ["branch", "-D", "gh-pages"], shell);
+    }
+    process.exit(exitCode);
+}
+
+async function scriptLinux() {
+    const {execa} = await import("execa");
+    let exitCode = 0;
+    try {
+        console.log("Building...");
+        await print(execa("npm", ["run", "build"]));
+
+        console.log("Pushing...");
+        await print(execa("git", ["checkout", "--orphan", "gh-pages"]));
+        await print(execa("git", ["--work-tree", "dist", "add", "--all"]));
+        await print(execa("git", ["--work-tree", "dist", "commit", "-m", "Deploy"]));
+        await print(execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]));
+
+        console.log("Successfully deployed");
+    } catch (e) {
+        console.error(e);
+        exitCode = 1;
+    } finally {
+        await execa("git", ["checkout", "-f", "main"]);
+        await execa("git", ["branch", "-D", "gh-pages"]);
+    }
+    process.exit(exitCode);
+}
+
+if (os === "Windows_NT") {
+    scriptWin();
+} else if (os === "Linux") {
+    scriptLinux();
+} else {
+    console.error("Unsupported operating system:", os);
+}
