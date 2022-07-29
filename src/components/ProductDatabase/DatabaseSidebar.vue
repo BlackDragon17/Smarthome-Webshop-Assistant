0673b2935a9b12a64de426f2e2a93e010c1ceb44
Michal <BlackDragon17@users.noreply.github.com>
2022-07-29 17:14:42 +0200

    Add more filtering options

--- src/components/ProductDatabase/DatabaseSidebar.vue
+++ src/components/ProductDatabase/DatabaseSidebar.vue
@@ -12,7 +12,7 @@
                            name="category"
                            value="all"
                            v-model="selectedCategory"
-                           checked>
+                           @change="selectedType = ''">
                     <label for="filter-category-radio-1" class="filter-label form-check-label">All</label>
                 </div>
                 <div class="form-check">
@@ -21,7 +21,8 @@
                            class="filter-input form-check-input"
                            name="category"
                            value="hub"
-                           v-model="selectedCategory">
+                           v-model="selectedCategory"
+                           @change="selectedType = 'open'">
                     <label for="filter-category-radio-2" class="filter-label form-check-label">Hubs</label>
                 </div>
                 <div class="form-check">
@@ -30,7 +31,8 @@
                            class="filter-input form-check-input"
                            name="category"
                            value="light"
-                           v-model="selectedCategory">
+                           v-model="selectedCategory"
+                           @change="selectedType = 'bulb'">
                     <label for="filter-category-radio-3" class="filter-label form-check-label">Lights</label>
                 </div>
                 <div class="form-check">
@@ -39,13 +41,96 @@
                            class="filter-input form-check-input"
                            name="category"
                            value="sensor"
-                           v-model="selectedCategory">
+                           v-model="selectedCategory"
+                           @change="selectedType = ''">
                     <label for="filter-category-radio-4" class="filter-label form-check-label">Sensors</label>
                 </div>
                 <hr>
             </div>
 
-            <div class="filter-group">
+            <div class="filter-group" v-if="selectedCategory !== 'all'">
+                <h4 class="filter-group-heading">Type of {{ selectedCategory }}</h4>
+                <div v-if="selectedCategory === 'hub'">
+                    <div class="form-check">
+                        <input type="radio"
+                               id="filter-type-radio-1"
+                               class="filter-input form-check-input"
+                               name="type"
+                               value="open"
+                               v-model="selectedType">
+                        <label for="filter-type-radio-1" class="filter-label form-check-label">Open</label>
+                    </div>
+                    <div class="form-check">
+                        <input type="radio"
+                               id="filter-type-radio-2"
+                               class="filter-input form-check-input"
+                               name="type"
+                               value="proprietary"
+                               v-model="selectedType">
+                        <label for="filter-type-radio-2" class="filter-label form-check-label">Proprietary</label>
+                    </div>
+                </div>
+
+                <div v-else-if="selectedCategory === 'light'">
+                    <div class="form-check">
+                        <input type="radio"
+                               id="filter-type-radio-1"
+                               class="filter-input form-check-input"
+                               name="type"
+                               value="lamp"
+                               v-model="selectedType">
+                        <label for="filter-type-radio-1" class="filter-label form-check-label">Lamp</label>
+                    </div>
+                    <div class="form-check">
+                        <input type="radio"
+                               id="filter-type-radio-2"
+                               class="filter-input form-check-input"
+                               name="type"
+                               value="bulb"
+                               v-model="selectedType">
+                        <label for="filter-type-radio-2" class="filter-label form-check-label">Bulb</label>
+                    </div>
+                    <div class="form-check">
+                        <input type="radio"
+                               id="filter-type-radio-3"
+                               class="filter-input form-check-input"
+                               name="type"
+                               value="lightStrip"
+                               v-model="selectedType">
+                        <label for="filter-type-radio-3" class="filter-label form-check-label">Light strip</label>
+                    </div>
+                </div>
+
+                <div v-else-if="selectedCategory === 'sensor'">
+                    Placeholder
+                </div>
+                <hr>
+            </div>
+
+            <div class="filter-group" v-if="selectedType === 'bulb'">
+                <h4 class="filter-group-heading">Form factor</h4>
+                <div class="form-check">
+                    <input type="radio"
+                           id="filter-form-factor-radio-1"
+                           class="filter-input form-check-input"
+                           name="formFactor"
+                           value="e14"
+                           v-model="selectedFormFactor">
+                    <label for="filter-form-factor-radio-1" class="filter-label form-check-label">E14</label>
+                </div>
+                <div class="form-check">
+                    <input type="radio"
+                           id="filter-form-factor-radio-2"
+                           class="filter-input form-check-input"
+                           name="formFactor"
+                           value="e27"
+                           v-model="selectedFormFactor">
+                    <label for="filter-form-factor-radio-2" class="filter-label form-check-label">E27</label>
+                </div>
+                <hr>
+            </div>
+
+            <div class="filter-group" v-if="selectedCategory === 'light'">
                 <h4 class="filter-group-heading">Features</h4>
                 <div class="form-check">
                     <input type="checkbox"
@@ -76,12 +161,21 @@
 
             <div class="filter-group">
                 <h4 class="filter-group-heading">Brand</h4>
+                <div class="form-check">
+                    <input type="checkbox"
+                           id="filter-brand-checkbox-all"
+                           class="filter-input form-check-input"
+                           v-model="selectedAllBrands"
+                           @change="checkboxAllBrands">
+                    <label for="filter-brand-checkbox-all" class="filter-label form-check-label">Any</label>
+                </div>
                 <div class="form-check" v-for="(brand, index) in allBrands" :key="brand">
                     <input type="checkbox"
                            :id="`filter-brand-checkbox-${index}`"
                            class="filter-input form-check-input"
                            :value="brand"
-                           v-model="selectedBrands">
+                           v-model="selectedBrands"
+                           @change="checkboxBrand">
                     <label :for="`filter-brand-checkbox-${index}`" class="filter-label form-check-label">{{ brand }}</label>
                 </div>
                 <hr>
@@ -96,8 +190,11 @@ export default {
 
     data() {
         return {
-            selectedCategory: "all",
+            selectedCategory: "light",
+            selectedType: "",
+            selectedFormFactor: "",
             selectedFeatures: [],
+            selectedAllBrands: true,
             selectedBrands: []
         };
     },
@@ -109,6 +206,21 @@ export default {
     computed: {
         filterListBorder() {
             return import.meta.env.PROD || this.hideBorders ? "none" : "1px solid darkgoldenrod";
+        }
+    },
+
+    methods: {
+        test() {
+            console.log("clicked");
+        },
+
+        // Checkbox callbacks
+        checkboxAllBrands() {
+            this.selectedAllBrands = true;
+            this.selectedBrands = [];
+        },
+        checkboxBrand() {
+            this.selectedAllBrands = this.selectedBrands.length <= 0;
         }
     }
 };
