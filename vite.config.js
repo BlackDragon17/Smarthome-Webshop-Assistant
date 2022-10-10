73f041e03a45dd81325e8cff7171520f0a251645
Michal <BlackDragon17@users.noreply.github.com>
2022-10-10 18:49:45 +0200

    Remove debug log in prod build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
@@ -9,9 +9,12 @@ export default defineConfig(({mode}) => {
         plugins: [vue()],
         resolve: {
             alias: {
-                '@': fileURLToPath(new URL('./src', import.meta.url))
+                "@": fileURLToPath(new URL("./src", import.meta.url))
             }
         },
-        base: mode === 'production' ? '/Smarthome-Webshop-Assistant/' : '/'
-    }
-})
+        base: mode === "production" ? "/Smarthome-Webshop-Assistant/" : "/",
+        esbuild: {
+            drop: ["console"]
+        }
+    };
+});
